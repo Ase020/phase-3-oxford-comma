ae1b1233054c0fc30b42f0f5a82eec76776b7a1a
felixNyalenda <felixasejunior@gmail.com>
2023-05-17 17:32:34 +0300

    completed assignment

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+  case array.size
+  when 0
+    ''
+  when 1
+    array[0]
+  when 2
+    "#{array[0]} and #{array[1]}"
+  else
+    last_element = array.pop
+    "#{array.join(', ')}, and #{last_element}"
+  end
 end
+
+print oxford_comma %w[fiddleheads okra kohlrabi]
